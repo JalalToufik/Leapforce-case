59859676bd63a8910b716dd5e1997a35275a283a
JalalToufik <112856590+JalalToufik@users.noreply.github.com>
2024-01-21 17:37:06 +0100

    mobile opzetje af

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,95 @@
-<h1>Mandatory title about the call-to-action here</h1>
+<p>
+    Label
+</p>
+
+<h1>
+    Mandatory title about the 
+    <span>call-to-action here</span>
+</h1>
+
+<p>
+    Optional body copy goes here.
+</p>
+
+<a href="/"> Optional button </a>
+
+<ul>
+    <li>
+        <div>
+            <p>Title</p>
+            <p>Supporting text about the call-to-action goes here.</p>
+        </div>
+        <button></button>
+    </li>
+    <li>
+        <div>
+            <p>Title</p>
+            <p>Supporting text about the call-to-action goes here.</p>
+        </div>
+        <button></button>
+    </li>
+</ul>
+
+
+<style>
+
+    * {
+        box-sizing: border-box;
+    }
+
+    p, h1 {
+        text-align: center;
+    }
+
+    a{
+        color: #BF564D;
+        display: block;
+        text-align: center;
+        margin: 1em 0;
+        text-decoration: none;
+    }
+
+    ul{
+        padding: 0;
+        margin: 0;
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+        gap: 1em;
+        list-style: none;
+    }
+
+    li{
+        background-image: url(/couple.png);
+        background-size: cover;
+        background-repeat: no-repeat;
+        width: 18em;
+        display: flex;
+        display: flex;
+        align-items: flex-end;
+        height: 69vw;
+        border-radius: 5px;
+        padding: 1em
+    }
+
+    li p{
+        color: white;
+        text-align: left;
+        margin: 0;
+    }
+
+    li p:nth-of-type(1){
+        padding-bottom: 1em;
+    }
+
+    button{
+        background-color: transparent;
+        border: solid 3px grey;
+        border-radius: 5px;
+        width: 2em;
+        height: 2em;
+    }
+
+
+
+</style>
